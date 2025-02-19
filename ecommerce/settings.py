e99f0db2f3ed408d62dfe8df34b318f47d14c3e4
MuhammedValeedK <153316369+MuhammedValeedK@users.noreply.github.com>
2025-02-19 12:01:27 +0530

    new1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'whitenoise.runserver_nostatic',
+    'storages',
     
      
 ]
@@ -196,7 +197,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = 'movadh'
-AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
