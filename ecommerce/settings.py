81ef09c454645eca7ac66549a05b01bfeefc1a89
MuhammedValeedK <153316369+MuhammedValeedK@users.noreply.github.com>
2025-02-19 17:01:34 +0530

    new updates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Allowed Hosts
-ALLOWED_HOSTS =["localhost","127.0.0.1","13.61.154.233"]
+ALLOWED_HOSTS =["localhost","127.0.0.1","51.20.120.19"]
 
 
 # Application definition
